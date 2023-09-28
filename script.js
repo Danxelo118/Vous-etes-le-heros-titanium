060a2df313627e16a2c256c77429be4878d729e0
Danxelo118 <danieldezemma@gmail.com>
2023-09-28 12:43:57 -0400

    version1

---
+++ script.js
@@ -0,0 +1,41 @@
+ chapters = {
+ debut : {
+    titre : "Le commencement" ,
+    description : [],
+    image : city,
+    boutons : [],
+},
+ declencheur : {
+    titre : "Une planète changée" ,
+    description : [],
+    image : city ,
+    boutons : [],
+ },
+ atterissage : {
+    titre : "L'arrivée ",
+    description : [],
+    image : [],
+    boutons : [],
+ },
+ base : {
+    titre : [],
+    description : [],
+    image : [],
+    boutons : [],
+ },
+ chemin : {
+    titre : [],
+    description : [],
+    image : [],
+    boutons : [],
+ },
+};
+
+ function goToChapter(chapter) {
+    // Si le chapitre existe 
+    //On affiche avec la console.log() son titre, sa description et ses boutons (clés)
+    // Sinon 
+    // Afficher un message d'erreur
+ }
+
+ // Ici on doit appeler la fonction pour faire apparaître le premier chapitre au chargement de la page.
